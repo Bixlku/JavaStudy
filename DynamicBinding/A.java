65cb9fb5d3a90a42e6e12fcb8ece7eca775b12e3
Bixlku <ike172345@gmail.com>
2022-02-07 00:09:32 +0800

    修改代码，更加体现动态绑定机制

--- DynamicBinding/A.java
+++ DynamicBinding/A.java
@@ -20,15 +20,15 @@ public class A {
 class B extends A {
     public int i = 20;
 
-    public int sum() {
-        return i + 20;
-    }
+    // public int sum() {
+    //     return i + 20;
+    // }
 
     public int getI() {
         return i;
     }
 
-    public int sum1() {
-        return i + 10;
-    }
+    // public int sum1() {
+    //     return i + 10;
+    // }
 }
--- DynamicBinding/DynamicBinding.java
+++ DynamicBinding/DynamicBinding.java
@@ -3,8 +3,8 @@ package com.poly;
 public class DynamicBinding {
     public static void main(String[] args) {
         A a = new B();
-        System.out.println(a.sum());
-        System.out.println(a.sum1());
+        System.out.println(a.sum());//40->30
+        System.out.println(a.sum1());//30->20
 
     }
 }
